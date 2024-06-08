4f9c30eecdc92f43d625f9dc9aacf891cbb3882a
Camden Rush <rush0camden@gmail.com>
2024-06-07 23:27:25 -0500

    remove dark:

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -34,8 +34,8 @@ function SocialLink({
 function Footer() {
     return (
         <footer className='mx-auto space-y-10 pb-16'>
-            <div className='flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row'>
-                <p className='text-xs text-zinc-600 dark:text-zinc-400'>
+            <div className='flex flex-col items-center justify-between gap-5 border-t pt-8 border-white/5 sm:flex-row'>
+                <p className='text-xs text-zinc-400'>
                     &copy; Copyright {new Date().getFullYear()} notaviruslol.com. All rights reserved.
                 </p>
                 <div className='flex gap-4'>
