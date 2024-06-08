b7c3c671f5be3c631e693456a88f315741902a89
Camden Rush <rush0camden@gmail.com>
2024-06-08 13:25:46 -0500

    fuck copyright

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -36,11 +36,11 @@ function Footer() {
         <footer className='mx-auto space-y-10 pb-16'>
             <div className='flex flex-col items-center justify-between gap-5 border-t pt-8 border-white/5 sm:flex-row'>
                 <p className='text-xs text-zinc-400'>
-                    &copy; Copyright {new Date().getFullYear()} notaviruslol.com. All rights reserved.
+                    &copy; {new Date().getFullYear()} notaviruslol.com. Licensed under the WTFPL.
                 </p>
                 <div className='flex gap-4'>
-                    <SocialLink href='#' icon={GitHubIcon}>
-                        Follow us on GitHub
+                    <SocialLink href='https://github.com/camden-git/cps-notavirus' icon={GitHubIcon}>
+                        Open Source on GitHub
                     </SocialLink>
                 </div>
             </div>
